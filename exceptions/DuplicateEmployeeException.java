bb39a42ce80ac2d653dc8a2bc517c7d2e5a6bed8
riddhibahuguna06 <riddhibahuguna@gmail.com>
2026-04-05 15:49:16 +0530

    Add DuplicateEmployeeException

--- exceptions/DuplicateEmployeeException.java
+++ exceptions/DuplicateEmployeeException.java
@@ -1,9 +1,28 @@
 package exceptions;
 
 public class DuplicateEmployeeException extends Exception {
+
+    private String employeeId;
+
     public DuplicateEmployeeException(){
         super("Employee already registered");
     }
 
-    public Duplicate
+    public DuplicateEmployeeException(String message){
+        super(message) ;
+    }
+
+   public DuplicateEmployeeException(String message , String employeeId){
+    super(employeeId + " Already exists " + message) ;
+
+    this.employeeId = employeeId ;
+   }
+
+   public DuplicateEmployeeException(String message, Throwable cause){
+     super(message, cause);
+   }
+
+   public String getEmployeeId(){
+    return employeeId ;
+   }
 }
